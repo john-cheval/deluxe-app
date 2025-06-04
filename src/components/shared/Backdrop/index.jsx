49c2ccdaf46322b9e1bce759d8d336c78f90f168
john-cheval <johncheval1@gmail.com>
2025-06-04 22:48:08 +0400

    gallery has been done

--- src/components/shared/Backdrop/index.jsx
+++ src/components/shared/Backdrop/index.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const Backdrop = ({ children, onClick }) => {
   return (
     <motion.div
-      className="fixed top-0 left-0 h-full w-full bg-black/[0.7] backdrop-blur-[1.5px]- flex items-center justify-center z-[100]"
+      className="fixed top-0 left-0 h-full w-full bg-black/[0.7] backdrop-blur-[1.5px]- flex items-center justify-center z-[100001]"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
